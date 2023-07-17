013c9ad2f89997710e4fafb5751c190cefd82d09
Dirk Jäckel <dirk@safe.global>
2023-07-17 09:34:31 +0200

    Remove debug statements

--- Multisig/UI/App/MainTabBarViewController.swift
+++ Multisig/UI/App/MainTabBarViewController.swift
@@ -105,13 +105,6 @@ class MainTabBarViewController: UITabBarController {
             object: nil)
 
         WebConnectionController.shared.attach(observer: self)
-
-        App.shared.snackbar.show(message: "infuraKey.isEmpty: \(App.configuration.services.infuraKey.isEmpty)")
-        App.shared.snackbar.show(message: "intercomAppId.isEmpty: \(App.configuration.services.intercomAppId.isEmpty)")
-        App.shared.snackbar.show(message: "intercomApiKey.isEmpty: \(App.configuration.services.intercomApiKey.isEmpty)")
-        App.shared.snackbar.show(message: "walletConnectProjectId.isEmpty: \(App.configuration.walletConnect.walletConnectProjectId.isEmpty)")
-        App.shared.snackbar.show(message: "googleClientId.isEmpty: \(App.configuration.web3auth.googleClientId.isEmpty)")
-        
     }
 
     deinit {
